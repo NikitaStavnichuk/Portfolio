92db4fa4a91594f8c53cb1ce16578299ac29ee88
Никита Ставничук <123439326+NikitaStavnichuk@users.noreply.github.com>
2024-02-10 16:44:38 +0300

    добавление раздела

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var infoElement = document.querySelector('.info');
+    var descriptionElement = document.querySelector('.description');
+
+    setTimeout(function() {
+        infoElement.classList.add('animate-fade-in-left');
+    }, 780);
+
+    setTimeout(function() {
+        descriptionElement.classList.add('animate-fade-in-left1');
+    }, 1000);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector('.contacts').classList.add('show');
+    }, 1000); 
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector('.skills1').classList.add('show1');
+    }, 800); 
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector('.skills2').classList.add('show2');
+    }, 800); 
+});
