ee918195c88bdc24723c8e30c94c896ee7d162cd
NikitaStavnichuk <pota1200@mail.ru>
2024-04-06 14:27:14 +0300

    Небольшие изменения :)

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.info').classList.add('show3');
-    }, 780); 
-});
+ document.addEventListener("DOMContentLoaded", function () {
+     setTimeout(function () {
+         document.querySelector('.info').classList.add('show3');
+     }, 780); 
+ });
 
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.description').classList.add('show4');
-    }, 1000); 
-});
+     setTimeout(function () {
+         document.querySelector('.description').classList.add('show4');
+     }, 1000); 
+ });
 
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.contacts').classList.add('show');
-    }, 1000); 
-});
+ document.addEventListener("DOMContentLoaded", function () {
+     setTimeout(function () {
+         document.querySelector('.contacts').classList.add('show');
+     }, 1000); 
+ });
 
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.skills1').classList.add('show1');
-    }, 800); 
-});
+ document.addEventListener("DOMContentLoaded", function () {
+     setTimeout(function () {
+         document.querySelector('.skills1').classList.add('show1');
+     }, 800); 
+ });
 
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.operation').classList.add('show2');
-    }, 800); 
+ document.addEventListener("DOMContentLoaded", function () {
+     setTimeout(function () {
+      document.querySelector('.operation').classList.add('show2');
+}, 800); 
 });
 
 
